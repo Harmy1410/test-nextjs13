c8f9f503920ef4394bc3285a15b44bb8c50a61c5
Harmy1410 <harmy1410@gmail.com>
2022-10-29 17:13:35 -0400

    working

--- app/pokemons/[pokeName]/page.tsx
+++ app/pokemons/[pokeName]/page.tsx
@@ -1,22 +1,11 @@
 import { use } from 'react';
 import getPokemon, { Pokemon } from '../../PokemonData';
 
-export default async function PokePage({
-    params,
-}: {
-    params: { pokeName: string } | undefined;
-}) {
-    if (!params) {
-        return (
-            <div>
-                <h1>Something went wrong</h1>
-            </div>
-        );
-    }
-    let pokemon: Pokemon = use(getPokemon(params.pokeName));
+export default async function PokePage({ params }: any) {
+    console.log(params.pokeName);
     return (
         <div style={{ padding: '2em' }}>
-            <h1>{JSON.stringify(pokemon, undefined, 2)}</h1>
+            <h1>{params.pokeName}</h1>
         </div>
     );
 }
